1e7e97bb4304f419d520177dd1ca759630c694d5
danvasquesc <dan.vasques@outlook.com.br>
2025-06-04 15:47:19 -0300

    feat: estrutura inicial do projeto com Spring Boot

---
+++ src/main/java/com/dan/atividadeescolar/AtividadeEscolarApplication.java
@@ -0,0 +1,13 @@
+package com.dan.atividadeescolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeEscolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeEscolarApplication.class, args);
+	}
+
+}
